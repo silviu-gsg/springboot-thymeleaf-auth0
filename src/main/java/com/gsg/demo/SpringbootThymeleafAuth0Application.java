264036cb6a181e7d09d8929ef7ac556b8d85e96b
Silviu Gheorghe <silviu.gheorghe@tremend.com>
2021-02-25 13:22:48 +0200

    initial commit

---
+++ src/main/java/com/gsg/demo/SpringbootThymeleafAuth0Application.java
@@ -0,0 +1,13 @@
+package com.gsg.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleafAuth0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootThymeleafAuth0Application.class, args);
+	}
+
+}
